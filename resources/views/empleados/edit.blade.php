9bc601b1be9ec9aedd00f811f41f0b445dd13a90
Sebas <sebascaballero77@gmail.com>
2018-10-07 19:01:51 -0300

    Request
    Se agrego las validaciones del abmde empleado

--- resources/views/empleados/edit.blade.php
+++ resources/views/empleados/edit.blade.php
@@ -15,7 +15,7 @@
         </div>
 
         <div class="panel-body">
-          <div class="col-md-2">
+          <div class="col-md-2 col-md-offset-2">
             <label for="func">Primer Nombre</label>
             <div class="input-group">
               <input type="text" name="primerNombre" class="form-control" value="{{ $empleado->primerNombre }}" placeholder="Primer Nombre del empleado">
@@ -39,8 +39,21 @@
               <input type="text" name="segundoApellido" class="form-control" value="{{ $empleado->segundoApellido }}" placeholder="Segundo Apellido">
             </div>
           </div>
+          <div class="col-md-2 col-md-offset-2">
+                <label for=""  style="margin-top: 10px">Dirección</label>
+                <div class="form-group">
+                  <input type="text" size="35" class="form-control" name="direccion" value="{{ $empleado->direccion }}" placeholder="Dirección">
+                </div>
+              </div>
+
+              <div class="col-md-2">
+                <label for="" style="margin-top: 10px">Teléfono</label>
+                <div class="form-group">
+                  <input type="text" size="19"  name="telefono" class="form-control" value="{{ $empleado->telefono }}" placeholder="Teléfono">
+                </div>
+              </div>
           <div class="col-md-2">
-            <label for="sel1" >Rubro</label>
+            <label for="sel1" style="margin-top: 10px">Rubro</label>
             <div class="form-group">
               <select class="form-control" id="rubro_id" name="rubro_id">
                 @foreach ($rubros as $rubro)
